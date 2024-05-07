03e4ea89f1c59eceb8f641d417cc03067ecd3d21
Joao Vitor Oliveira <jv.oliveraraujo@gmail.com>
2024-05-07 18:34:14 -0300

    add styles to cart

--- src/app/cart-page/cart-page.component.ts
+++ src/app/cart-page/cart-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CartService } from '../services/cart/cart.service';
 import { Cart } from '../shared/models/Cart';
 import { CartItem } from '../shared/models/CartItem';
+import { FoodService } from '../services/food/food.service';
 
 @Component({
   selector: 'app-cart-page',
@@ -11,7 +12,10 @@ import { CartItem } from '../shared/models/CartItem';
 export class CartPageComponent implements OnInit {
   cart!:Cart;
 
-  constructor(private cartService: CartService) { 
+  constructor(private cartService: CartService, ) { 
+    cartService.addToCart(foods[1]);
+    cartService.addToCart(foods[3]);
+    cartService.addToCart(foods[5]);
     this.setCart();
   }
 
