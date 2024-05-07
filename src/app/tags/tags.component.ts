edfa2c8fea55e80bc31b59c7b6f29616d6a9b46f
Joao Vitor Oliveira <jv.oliveraraujo@gmail.com>
2024-05-07 16:50:54 -0300

    add food page component

--- src/app/tags/tags.component.ts
+++ src/app/tags/tags.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Tag } from '../shared/models/Tag';
 import { FoodService } from '../services/food/food.service';
 
@@ -8,12 +8,15 @@ import { FoodService } from '../services/food/food.service';
   styleUrls: ['./tags.component.css']
 })
 export class TagsComponent implements OnInit {
-
-  tags:Tag[] = [];
+  @Input() 
+  foodPageTags?:string[]
+  tags?:Tag[];
   constructor(private foodService:FoodService) { }
 
   ngOnInit(): void {
-    this.tags = this.foodService.getAllTags();
+    if(!this.foodPageTags){
+      this.tags = this.foodService.getAllTags();
+    }
   }
 
 }
